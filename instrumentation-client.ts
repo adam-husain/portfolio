f52db44f5cf6efca8310e15ea149b3ddf60a312c
Adam Husain <adamhusain12@gmail.com>
2026-02-03 12:02:32 +0800

    Configured ENV variables

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,8 +1,8 @@
 import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+  ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST,
   // Include the defaults option as required by PostHog
   defaults: "2025-11-30",
   // Enables capturing unhandled exceptions via Error Tracking
--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,11 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/ingest/static/:path*",
+        source: "/portfolio/data/static/:path*",
         destination: "https://us-assets.i.posthog.com/static/:path*",
       },
       {
-        source: "/ingest/:path*",
+        source: "/portfolio/data/:path*",
         destination: "https://us.i.posthog.com/:path*",
       },
     ];
